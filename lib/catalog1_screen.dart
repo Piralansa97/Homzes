77190f312c6f66d5ff45af4e88642f380bf1cec8
Piralansa97 <piralansa@gmail.com>
2025-04-16 18:13:30 +0530

    added new changes

--- lib/catalog1_screen.dart
+++ lib/catalog1_screen.dart
@@ -3,33 +3,113 @@ import 'package:flutter/material.dart';
 class Catalog1Screen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        title: Text('Hi, Stanislav'),
-        actions: [CircleAvatar(child: Text('S')), SizedBox(width: 16)],
-      ),
-      body: ListView(
-        padding: EdgeInsets.all(16),
-        children: [
-          TextField(
-            decoration: InputDecoration(
-              hintText: 'Search',
-              prefixIcon: Icon(Icons.search),
-              border:
-                  OutlineInputBorder(borderRadius: BorderRadius.circular(12)),
-              filled: true,
-              fillColor: Colors.grey[200],
+    final screenHeight = MediaQuery.of(context).size.height;
+    final screenWidth = MediaQuery.of(context).size.width;
+    return SafeArea(
+      child: Scaffold(
+        extendBodyBehindAppBar: true,
+        body: ListView(
+          //padding: EdgeInsets.all(16),
+          children: [
+            Container(
+              // padding: EdgeInsets.only(
+              //   left: screenWidth, // 5% of screen width
+              //   right: screenWidth * 0.05,
+              //   top: screenHeight * 0.03, // 3% of screen height
+              //   bottom: screenHeight * 0.03,
+              // ),
+              // width: double.infinity,
+              // height: screenHeight * 0.3,
+              padding: const EdgeInsets.only(
+                  top: 70, left: 16, right: 16, bottom: 16),
+              decoration: BoxDecoration(
+                color: Color.fromARGB(255, 240, 242, 152), // Light yellow
+                borderRadius:
+                    BorderRadius.vertical(bottom: Radius.circular(30)),
+              ),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Icon(Icons.menu, color: Colors.black),
+                      Row(
+                        children: [
+                          Text(
+                            "Hi, Stanislav",
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                          const SizedBox(width: 10),
+                          Stack(
+                            children: [
+                              CircleAvatar(
+                                radius: 15,
+                                backgroundColor: Colors.grey[400],
+                                child: Text("S",
+                                    style: TextStyle(color: Colors.white)),
+                              ),
+                              Positioned(
+                                top: 0,
+                                right: 0,
+                                child: Container(
+                                  width: 8,
+                                  height: 8,
+                                  decoration: BoxDecoration(
+                                    color: Colors.pink,
+                                    shape: BoxShape.circle,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+                  TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Search',
+                      prefixIcon: Icon(Icons.search),
+                      contentPadding:
+                          EdgeInsets.symmetric(vertical: 0, horizontal: 16),
+                      filled: true,
+                      fillColor: Colors.white,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(25),
+                        borderSide: BorderSide.none,
+                      ),
+                    ),
+                  )
+                ],
+              ),
             ),
-          ),
-          SizedBox(height: 24),
-          _sectionTitle(context, 'Featured'),
-          _horizontalList(),
-          SizedBox(height: 24),
-          Text('New offers',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-          _horizontalList(),
-        ],
+            SizedBox(height: 24),
+            // Padding(
+            //   padding: EdgeInsets.only(
+            //     left: screenWidth, // 5% of screen width
+            //     right: screenWidth * 0.05,
+            //     top: screenHeight * 0.03, // 3% of screen height
+            //     bottom: screenHeight * 0.03,
+            //   ),
+            // ),
+            Padding(
+              padding: EdgeInsets.all(16),
+              child: Column(
+                children: [
+                  _sectionTitle(context, 'Featured'),
+                  _horizontalList(),
+                  SizedBox(height: 24),
+                  _sectionTitle(context, 'New Offers'),
+                  // Text('New offers',
+                  //     style:
+                  //         TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                  _horizontalList(),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -42,7 +122,8 @@ class Catalog1Screen extends StatelessWidget {
             style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
         GestureDetector(
           onTap: () => Navigator.pushNamed(context, '/catalog3'),
-          child: Text('View all', style: TextStyle(color: Colors.blue)),
+          child: Text('View all',
+              style: TextStyle(color: const Color.fromARGB(255, 49, 49, 49))),
         ),
       ],
     );
@@ -57,7 +138,7 @@ class Catalog1Screen extends StatelessWidget {
           width: 160,
           decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(12),
-            color: Colors.white,
+            color: const Color.fromARGB(255, 90, 86, 86),
             boxShadow: [BoxShadow(color: Colors.grey.shade200, blurRadius: 4)],
           ),
           child: Column(
--- lib/catalog3_screen.dart
+++ lib/catalog3_screen.dart
@@ -22,9 +22,146 @@ class Catalog3Screen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Popular rent offers')),
-      body: ListView.builder(
+    return SafeArea(
+      child: Scaffold(
+        body: ListView(
+          children: [
+            Container(
+              padding: const EdgeInsets.only(
+                  top: 70, left: 16, right: 16, bottom: 16),
+              decoration: BoxDecoration(
+                color: Color.fromARGB(255, 198, 231, 190), // Light yellow
+                borderRadius:
+                    BorderRadius.vertical(bottom: Radius.circular(30)),
+              ),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        width: 48,
+                        height: 48,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          //border: Border.all(color: Colors.white, width: 2),
+                          color: Colors.black, // transparent fill
+                        ),
+                        child: IconButton(
+                          icon: Icon(Icons.menu),
+                          color: Colors.white, // icon color to match the border
+                          onPressed: () {
+                            // action
+                          },
+                        ),
+                      ),
+
+                      SizedBox(width: 10), // optional spacing
+                      Expanded(
+                        child: TextField(
+                          decoration: InputDecoration(
+                            hintText: 'Search',
+                            prefixIcon: Icon(Icons.search),
+                            contentPadding: EdgeInsets.symmetric(
+                                vertical: 0, horizontal: 16),
+                            filled: true,
+                            fillColor: Colors.white,
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(25),
+                              borderSide: BorderSide.none,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              "Popular rent offers",
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            Padding(
+              padding: EdgeInsets.all(16),
+              child: Column(
+                children: [
+                  _verticalList(),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+    //     appBar: AppBar(title: Text('Popular rent offers')),
+    //     body: ListView.builder(
+    //       itemCount: properties.length,
+    //       padding: EdgeInsets.all(16),
+    //       itemBuilder: (_, index) {
+    //         final prop = properties[index];
+    //         return Card(
+    //           shape:
+    //               RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+    //           margin: EdgeInsets.only(bottom: 16),
+    //           child: Column(
+    //             crossAxisAlignment: CrossAxisAlignment.start,
+    //             children: [
+    //               ClipRRect(
+    //                 borderRadius: BorderRadius.vertical(top: Radius.circular(12)),
+    //                 child: Image.asset(prop['image'], fit: BoxFit.cover),
+    //               ),
+    //               Padding(
+    //                 padding: const EdgeInsets.all(12.0),
+    //                 child: Column(
+    //                   crossAxisAlignment: CrossAxisAlignment.start,
+    //                   children: [
+    //                     Row(
+    //                       children: [
+    //                         _iconLabel('${prop['beds']} Beds'),
+    //                         SizedBox(width: 10),
+    //                         _iconLabel('${prop['baths']} Bathroom'),
+    //                       ],
+    //                     ),
+    //                     SizedBox(height: 8),
+    //                     Text(prop['title'],
+    //                         style: TextStyle(fontWeight: FontWeight.bold)),
+    //                     Text(prop['location'],
+    //                         style: TextStyle(color: Colors.grey[600])),
+    //                     SizedBox(height: 8),
+    //                     Text('\$${prop['price']} / mo',
+    //                         style: TextStyle(fontWeight: FontWeight.bold)),
+    //                   ],
+    //                 ),
+    //               )
+    //             ],
+    //           ),
+    //         );
+    //       },
+    //     ),
+    //   );
+    // }
+
+    // Widget _iconLabel(String label) {
+    //   return Container(
+    //     padding: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+    //     decoration: BoxDecoration(
+    //       color: Colors.grey[200],
+    //       borderRadius: BorderRadius.circular(8),
+    //     ),
+    //     child: Text(label),
+    //   );
+    // }
+  }
+
+  Widget _verticalList() {
+    return Container(
+      height: 200,
+      child: ListView.builder(
         itemCount: properties.length,
         padding: EdgeInsets.all(16),
         itemBuilder: (_, index) {
